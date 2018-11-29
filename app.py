e69656ba8afad284af2efe0e02ab68a7d1e2e2b8
Sam Joseph <sam.joseph@blackcatsolutions.co.uk>
2018-11-29 16:56:04 +0000

    initial commit with simple stacked bar

---
+++ app.py
@@ -0,0 +1,40 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import data
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = data.two_week_wait_data()
+
+
+def providers_by_wait(key):
+    grouped = df[['Provider', key]].groupby('Provider')
+    return go.Bar(
+        x=list(grouped.groups.keys()),
+        y=grouped.sum().sort_values(key)[key],
+        name=key
+    )
+
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='Mean wait',
+        figure={
+            'data': [
+                providers_by_wait('Within 14 days'),
+                providers_by_wait('15 to 16 days'),
+                providers_by_wait('17 to 21 days'),
+                providers_by_wait('22 to 28 days'),
+                providers_by_wait('After 28 days')
+            ],
+            'layout': go.Layout(barmode='stack')
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def two_week_wait_data():
+    xls = pd.ExcelFile(
+        '/Users/samjoseph/Downloads/SEPTEMBER-2018-CANCER-WAITING-TIMES-PROVIDER-WORKBOOK-FINAL-updated-XLSX-1MB.xlsx')
+    df = pd.read_excel(xls, 'TWO WEEK WAIT-BY CANCER',
+                       usecols='B:D,J:N', header=6)
+    df.columns = ['ODS', 'Provider', 'Cancer Type', 'Within 14 days',
+                  '15 to 16 days', '17 to 21 days', '22 to 28 days', 'After 28 days']
+    df = df[df.Provider != 'ALL ENGLISH PROVIDERS']
+
+    return df
